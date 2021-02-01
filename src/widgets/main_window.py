3c48f8b39bcd7f3c33534957da211d0fa7a3f840
EricsonWillians <ericson.willians@concil.com.br>
2021-02-01 20:03:48 -0300

    Added file menu with exit action

--- src/widgets/main_window.py
+++ src/widgets/main_window.py
@@ -1,9 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QApplication, qApp, QAction
 from src import const
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
@@ -12,6 +12,16 @@ class MainWindow(QWidget):
         self.resize(const.SCREEN_WIDTH, const.SCREEN_HEIGHT)
         self.center()
         self.setWindowTitle(const.MAIN_WINDOW_TITLE)
+
+        exit_action = QAction('&Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit application')
+        exit_action.triggered.connect(qApp.quit)
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+        file_menu.addAction(exit_action)
+
         self.show()
 
     def center(self):
