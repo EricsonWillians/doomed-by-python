8f5a9ac41c08363a0c2335e55d298440cfec0ddf
Ericson Willians <ericson.willians@concil.com.br>
2021-02-15 22:34:01 -0300

    Added wad listings example

--- src/widgets/wad_finder/__init__.py
+++ src/widgets/wad_finder/__init__.py
@@ -1,11 +1,19 @@
 from PyQt5.QtWidgets import *
 from src import const
+from bs4 import BeautifulSoup
+import requests
 
 
 class WadFinder(QWidget):
     def __init__(self):
         super().__init__()
         self.initUi()
+        r = requests.get(
+            'https://www.doomworld.com/idgames/levels/doom2/deathmatch/a-c/')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        wadListings = soup.findAll("table", {"class": "wadlisting"})
+
+        print(wadListings)
 
     def initUi(self):
         self.resize(const.SCREEN_WIDTH, const.SCREEN_HEIGHT)
@@ -13,3 +21,6 @@ class WadFinder(QWidget):
         self.grid = QGridLayout()
         self.setLayout(self.grid)
         self.error_dialog = QErrorMessage()
+
+    def findWads(self, searchWord):
+        pass
