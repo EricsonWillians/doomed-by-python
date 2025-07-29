55e46084c3b5a189a3ab7631e0f3fb7b392b588f
Ericson Willians <rederickdeathwill@gmail.com>
2025-07-29 17:55:07 -0300

    Add detailed mod info panel

--- src/widgets/pwad_list/__init__.py
+++ src/widgets/pwad_list/__init__.py
@@ -1,30 +1,82 @@
+import os
 from PyQt5.Qt import Qt
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import (
+    QTreeWidget,
+    QTreeWidgetItem,
+    QAbstractItemView,
+)
 
 
-class PWadList(QListWidget):
+def _file_size(path: str) -> str:
+    """Return file size in kilobytes formatted as a string."""
+    try:
+        size_kb = os.path.getsize(path) // 1024
+        return f"{size_kb} KB"
+    except OSError:
+        return "?"
+
+
+class PWadList(QTreeWidget):
 
     def __init__(self):
         super().__init__()
+        self.setColumnCount(3)
+        self.setHeaderLabels(["Mod", "Size", "Folder"])
+        self.setDragDropMode(QAbstractItemView.InternalMove)
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.setRootIsDecorated(False)
+        self.setToolTip('Drag to reorder mods. Delete key removes entries.')
+
+    def moveUp(self):
+        """Move the selected items up by one position."""
+        selected = self.selectedItems()
+        if not selected:
+            return
+        for item in selected:
+            idx = self.indexOfTopLevelItem(item)
+            if idx > 0:
+                self.takeTopLevelItem(idx)
+                self.insertTopLevelItem(idx - 1, item)
+                self.setCurrentItem(item)
+
+    def moveDown(self):
+        """Move the selected items down by one position."""
+        selected = self.selectedItems()
+        if not selected:
+            return
+        # process in reverse to avoid leapfrogging
+        for item in reversed(selected):
+            idx = self.indexOfTopLevelItem(item)
+            if idx < self.topLevelItemCount() - 1:
+                self.takeTopLevelItem(idx)
+                self.insertTopLevelItem(idx + 1, item)
+                self.setCurrentItem(item)
 
     def keyPressEvent(self, event):
-        for item in self.selectedItems():
-            current_row = self.row(item)
-            if event.key() == Qt.Key_Delete:
-                self.takeItem(current_row)
-            elif event.key() == Qt.Key_Down:
-                if current_row == len(self)-1:
-                    self.setCurrentRow(0)
-                else:
-                    self.setCurrentRow(current_row+1)
-            elif event.key() == Qt.Key_Up:
-                if current_row == 0:
-                    self.setCurrentRow(len(self)-1)
-                else:
-                    self.setCurrentRow(current_row-1)
+        if event.key() == Qt.Key_Delete:
+            for item in self.selectedItems():
+                index = self.indexOfTopLevelItem(item)
+                self.takeTopLevelItem(index)
+        else:
+            super().keyPressEvent(event)
 
     def getItems(self):
         items = []
-        for n in range(len(self)):
-            items.append(self.item(n))
+        for n in range(self.topLevelItemCount()):
+            items.append(self.topLevelItem(n))
         return items
+
+    def addWad(self, path: str):
+        """Add a wad entry with size information if not already present."""
+        paths = [i.data(0, Qt.UserRole) for i in self.getItems()]
+        if path in paths:
+            return False
+        item = QTreeWidgetItem([
+            os.path.basename(path),
+            _file_size(path),
+            os.path.dirname(path),
+        ])
+        item.setToolTip(0, path)
+        item.setData(0, Qt.UserRole, path)
+        self.addTopLevelItem(item)
+        return True
