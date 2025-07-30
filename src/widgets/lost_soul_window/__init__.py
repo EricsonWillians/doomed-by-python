cb4d718f1498ec18fa2cba2c13b3f92c2415fd6a
Ericson Willians <ericsonwillians@protonmail.com>
2025-07-30 14:15:06 -0300

    aesthetic upgrades

--- src/widgets/lost_soul_window/__init__.py
+++ src/widgets/lost_soul_window/__init__.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QProgressBar
-from PyQt5.QtGui import QMovie
+from PyQt5.QtGui import QMovie, QSize
 from PyQt5.Qt import Qt
 
 
@@ -7,19 +7,24 @@ class LostSoulWindow(QDialog):
     """A small window showing a loading animation."""
 
     def __init__(self, parent=None):
-        super().__init__(parent, Qt.WindowStaysOnTopHint)
+        flags = Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint
+        super().__init__(parent, flags)
+        self.setObjectName("lostSoulWindow")
         self.setWindowTitle("Loading")
-        layout = QVBoxLayout()
-        self.label = QLabel()
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(8, 8, 8, 8)
+
+        self.label = QLabel(self)
         movie = QMovie("assets/lost_soul.gif")
+        movie.setScaledSize(QSize(64, 64))
         self.label.setMovie(movie)
         movie.start()
         self.label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.label)
-        self.progress = QProgressBar()
+        layout.addWidget(self.label, alignment=Qt.AlignCenter)
+
+        self.progress = QProgressBar(self)
         self.progress.setRange(0, 100)
         layout.addWidget(self.progress)
-        self.setLayout(layout)
 
     def setRange(self, minimum: int, maximum: int):
         self.progress.setRange(minimum, maximum)
